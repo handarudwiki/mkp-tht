29978e330cfe75f9ece7b600cd85c703f6bc76f4
handarudwiki <handarudwiki04@gmail.com>
2025-07-01 18:05:58 +0700

    feat: fix type time ind terminal

--- internal/models/terminal.go
+++ internal/models/terminal.go
@@ -1,12 +1,14 @@
 package models
 
+import "time"
+
 type Terminal struct {
-	ID        int     `json:"id"`
-	Name      string  `json:"name"`
-	Location  string  `json:"location"`
-	Address   string  `json:"address"`
-	Latitude  float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	UpdatedAt string  `json:"updated_at"`
-	CreatedAt string  `json:"created_at"`
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Location  string    `json:"location"`
+	Address   string    `json:"address"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
